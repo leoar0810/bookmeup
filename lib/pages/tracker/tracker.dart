dfc3df9858c606c0ce54a6cf90353b85228f26ec
Miguel <ohyeahs4@hotmail.com>
2023-05-06 19:53:37 -0500

    tracker visual update

--- lib/pages/tracker/tracker.dart
+++ lib/pages/tracker/tracker.dart
@@ -20,9 +20,9 @@ class ReadingDashboardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 28, 117, 190),
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 28, 117, 190),
         elevation: 0,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
@@ -32,9 +32,9 @@ class ReadingDashboardWidget extends StatelessWidget {
           },
         ),
         title: Text(
-          'Bookmeup',
+          'Tracker',
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.white,
             fontWeight: FontWeight.bold,
             fontSize: 30.0,
           ),
@@ -46,41 +46,6 @@ class ReadingDashboardWidget extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Days of reading',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '$daysOfReading',
-                    style: TextStyle(fontSize: 36.0),
-                  ),
-                  SizedBox(height: 20),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Number of ranked books',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    rankedBooks.length.toString(),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 40),
               Expanded(
                 child: Row(
                   children: [
@@ -96,16 +61,95 @@ class ReadingDashboardWidget extends StatelessWidget {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            Icon(
+                              Icons.bookmark_added,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
                             Text(
-                              'books to read',
+                              'Pages read',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              '$daysOfReading',
+                              style: TextStyle(
+                                fontSize: 36.0,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10, bottom: 50, top: 50),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue[50],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.pending_actions,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
+                            Text(
+                              'Books to read',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              booksToRead.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 10, bottom: 50, top: 50),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue[50],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.book,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
+                            Text(
+                              'Read books',
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(height: 20),
                             Text(
-                              '25%',
+                              '0',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 30,
@@ -123,25 +167,29 @@ class ReadingDashboardWidget extends StatelessWidget {
                             EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
-                          color: Colors.purple[50],
+                          color: Colors.blue[50],
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            Icon(
+                              Icons.star_border,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
                             Text(
-                              'books to rank',
+                              'Ranked books',
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(height: 20),
                             Text(
-                              '75%',
+                              '0',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 30,
-                                color: Colors.purple,
+                                color: Colors.blue,
                               ),
                             ),
                           ],
