0a7f71064416d6b02f25f99cdb94f6086be73b12
Miguel <ohyeahs4@hotmail.com>
2023-05-07 13:56:31 -0500

    fixed discrepancy bug

--- lib/pages/tracker/tracker.dart
+++ lib/pages/tracker/tracker.dart
@@ -10,15 +10,15 @@ class ReadingDashboardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final pagesReaded = Get.arguments[0];
-    final booksReaded = Get.arguments[1];
-    final rankedBooks = Get.arguments[2];
-    final booksToRead = Get.arguments[3];
+    //final pagesReaded = Get.arguments[0];
+    //final booksReaded = Get.arguments[1];
+    //final rankedBooks = Get.arguments[2];
+    //final booksToRead = Get.arguments[3];
 
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 28, 117, 190),
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 28, 117, 190),
         elevation: 0,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
@@ -28,9 +28,9 @@ class ReadingDashboardWidget extends StatelessWidget {
           },
         ),
         title: Text(
-          'Bookmeup',
+          'Tracker',
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.white,
             fontWeight: FontWeight.bold,
             fontSize: 30.0,
           ),
@@ -42,41 +42,6 @@ class ReadingDashboardWidget extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Pages readed',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    pagesReaded.toString(),
-                    style: TextStyle(fontSize: 36.0),
-                  ),
-                  SizedBox(height: 20),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Number of books to read',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    booksToRead.toString(),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 40),
               Expanded(
                 child: Row(
                   children: [
@@ -92,16 +57,99 @@ class ReadingDashboardWidget extends StatelessWidget {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            Icon(
+                              Icons.bookmark_added,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
                             Text(
-                              'books readed',
+                              'Pages read',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              //pagesReaded.toString(),
+                              '0',
+                              style: TextStyle(
+                                fontSize: 36.0,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10, bottom: 50, top: 50),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue[50],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.pending_actions,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
+                            Text(
+                              'Books to read',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              //booksToRead.toString(),
+                              '0',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(right: 10, bottom: 50, top: 50),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue[50],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.book,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
+                            Text(
+                              'Read books',
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(height: 20),
                             Text(
-                              booksReaded.toString(),
+                              //booksReaded.toString(),
+                              '0',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 30,
@@ -119,25 +167,30 @@ class ReadingDashboardWidget extends StatelessWidget {
                             EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
-                          color: Colors.purple[50],
+                          color: Colors.blue[50],
                         ),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            Icon(
+                              Icons.star_border,
+                              color: Colors.blue,
+                              size: 50,
+                            ),
                             Text(
-                              'books ranked',
+                              'Ranked books',
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(height: 20),
                             Text(
-                              rankedBooks.toString(),
+                              //rankedBooks.toString(),
+                              '0',
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 30,
-                                color: Colors.purple,
+                                color: Colors.blue,
                               ),
                             ),
                           ],
